d260e0e4cde94133a93eb247935edc12d8685073
Daniel Afkhami <dan.afkhami@gmail.com>
2026-04-23 16:20:34 -0400

    Add hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,14 @@
 export const Hero = () => {
     return (
-      <section>
-        Hello World!
+      <section className="h-screen flex flex-col justify-center items-center">
+        <div className="text-center flex flex-col gap-4">
+          <h1 className="text-7xl md:text-8xl font-sans-serif font-bold">
+            Daniel Afkhami
+          </h1>
+          <p className="text-lg md:text-xl text-slate-600 font-medium">
+            Honors Math + CS @ Rutgers University
+          </p>
+        </div>
       </section>
     )
 }
