ad1db0699f0aa204280e5bf210bd9a3b4f67aabc
Daniel0359 <dan.afkhami@gmail.com>
2026-04-26 23:08:12 -0400

    Optimized text sizes and added wrapping

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,19 +1,16 @@
-import { FaLinkedin } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaEnvelope } from "react-icons/fa6";
+import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from "react-icons/fa6";
 
 export const Hero = () => {
     return (
       <section className="h-screen flex flex-col justify-center items-center">
         <div className="text-center flex flex-col gap-4">
-          <h1 className="text-7xl md:text-8xl font-sans-serif font-bold">
+          <h1 className="text-5xl md:text-7xl font-bold">
             Daniel Afkhami
           </h1>
-          <p className="text-lg md:text-xl text-slate-600 font-medium">
+          <p className="text-md md:text-lg text-slate-600 font-medium">
             Honors Math + CS @ Rutgers University
           </p>
-          <p className="text-lg md:text-xl text-sky-600 font-medium flex flex-row gap-8 md:gap-14 justify-center mt-6 md:mt-10">
+          <nav className="text-md md:text-lg text-sky-600 font-medium flex flex-wrap gap-6 md:gap-12 justify-center mt-6 md:mt-10 px-4">
             <a href = "https://www.linkedin.com/in/daniel-afkhami" target="_blank" rel="noopener noreferrer" className="flex flex-row gap-2 items-center">
             <FaLinkedin /> LinkedIn
             </a>
@@ -26,9 +23,8 @@ export const Hero = () => {
             <a href = "mailto:dan.afkhami@gmail.com" className="flex flex-row gap-2 items-center">
             <FaEnvelope /> Email
             </a>
-          </p>
+          </nav>
         </div>
-        
       </section>
     )
 }
