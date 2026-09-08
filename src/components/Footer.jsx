d35309ac5a683a4816b5b07b4a276d684c1b18f9
Daniel Afkhami <dan.afkhami@gmail.com>
2026-09-08 14:01:26 -0400

    Update Footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
     return (
         <footer 
-        className="w-full py-6 text-sm sm:text-base md:text-lg font-mono border border-slate-200 dark:border-slate-800 
-        text-slate-600 dark:text-slate-400 bg-white/80 dark:bg-slate-900/40 text-center"
+        className="w-full py-6 text-sm sm:text-base md:text-lg font-mono border-t border-slate-200 dark:border-slate-800 
+        text-slate-600 dark:text-slate-400 bg-slate-50 dark:bg-slate-950 text-center"
         >
             <p>&copy; {new Date().getFullYear()} Daniel Afkhami. All rights reserved.</p>
         </footer>
